3a19821fccc65959703cd989229988d8bbb78ec1
Palotás Boldizsár <boldizsar.palotas@gmail.com>
2023-07-09 17:25:57 +0200

    WIP

--- boldiweb.py
+++ boldiweb.py
@@ -37,7 +37,9 @@ class SourceFolder:
         return hash(self.path)
 
 
-def relative_path(target: Path, relative_to: Path) -> Path:
+def full_relative_to(target: Path, relative_to: Path) -> Path:
+    target = Path(target)  # actually accept str objects too
+    relative_to = Path(relative_to)
     for i, relative_to_parents in enumerate([relative_to] + list(relative_to.parents)):
         if target.is_relative_to(relative_to_parents):
             return Path(*([Path("..")] * i)) / target.relative_to(relative_to_parents)
@@ -45,10 +47,9 @@ def relative_path(target: Path, relative_to: Path) -> Path:
 
 @dataclass
 class Album:
-    root_folder: SourceFolder
+    source: SourceFolder
     target: Path
     env: jinja2.Environment = field(init=False)
-    index_template: jinja2.Template = field(init=False)
 
     def __post_init__(self):
         self.env = jinja2.Environment(
@@ -68,24 +69,22 @@ class Album:
             "album": self,
             "target": self.target,
         }
-        self.env.filters["relative_to"] = relative_path
-        self.index_template = self.env.get_template("index.html")
+        self.env.filters["relative_to"] = full_relative_to
 
     def render(self):
-        self.render_folder(self.root_folder)
+        self.render_folder(self.source)
 
     def render_folder(self, folder: SourceFolder):
-        stream = self.index_template.stream(
+        relative_path = full_relative_to(folder.path, self.source.path)
+        target_path = self.target / relative_path / "index.html"
+        target_path.parent.mkdir(parents=True, exist_ok=True)
+
+        index_template = self.env.get_template("index.html")
+        stream = index_template.stream(
             {
                 "folder": folder,
             }
         )
-        target_path = (
-            self.target
-            / relative_path(folder.path, self.root_folder.path)
-            / "index.html"
-        )
-        target_path.parent.mkdir(parents=True, exist_ok=True)
         with open(target_path, "w") as fp:
             stream.dump(fp)
 
