a31a837d7bdbfd883af508b5fe93d90b44328037
Palotás Boldizsár <boldizsar.palotas@gmail.com>
2023-07-13 00:26:31 +0200

    WIP. But need to solve build.db.json changing after 1st build due to sources becoming "built" targets

--- boldibuild.py
+++ boldibuild.py
@@ -80,43 +80,32 @@ class Build(abc.ABC):
     def register_dependency(self, target: Target, dependency: Target):
         dep_handler = self.get_handler(dependency)
         self.db.dependencies[target][dependency] = dep_handler.stamp(dependency)
+        self.db.dependencies[dependency]  # touch
 
-    def rebuild(self, target: Target) -> bool:
+    def rebuild(self, target: Target):
+        print(f"rebuild({target=!r})")
         handler = self.get_handler(target)
-        old_stamp = self.db.targets.pop(target, None)
-        _ = self.db.dependencies.pop(target, None)
+        self.db.dependencies.pop(target, None)
         handler.build_impl(target, partial(self.register_dependency, target))
         self.db.targets[target] = handler.stamp(target)
-        return old_stamp is not None and handler.stamps_match(old_stamp, self.db.targets[target])
 
-    def build(self, target: Target) -> bool:
-        print(f"build({target=!r})")
+    def build(self, target: Target):
+        # print(f"build({target=!r})")
         handler = self.get_handler(target)
         old_stamp = self.db.targets[target]
         cur_stamp = handler.stamp(target)
         if not handler.stamps_match(old_stamp, cur_stamp):
-            return self.rebuild(target)
+            self.rebuild(target)
+            return
 
         for dep, old_dep_stamp in self.db.dependencies[target].items():
-            # Do I care if the dep is rebuilt and gets a new stamp? This happens if e.g.:
-            #   trigger build 2a -> causes build 1a
-            #   commit 1a to 1b change
-            #   trigger build 1b (target 1 will new up-to-date stamp, but not 2a's dep 1)
-            #   commit 1b to 1a change
-            #   trigger build 2a -> causes build 1a
-            #       1 was not up to date, build(1) returns False
-            #       but stamp(1) matches old_dep_stamp
-            #       and 2a was built with 1a, so I do NOT need to build it again
-            # Ergo I don't care about dep's build results.
-            _ = self.build(dep)
-
+            self.build(dep)
             dep_handler = self.get_handler(dep)
             new_dep_stamp = dep_handler.stamp(dep)
             if not dep_handler.stamps_match(old_dep_stamp, new_dep_stamp):
-                return self.rebuild(target)
+                self.rebuild(target)
+                return
 
-        # the earlier handler.stamps_match(old_stamp, cur_stamp) call returned True
-        return True
 
     def load_build_db(self):
         self.db.load(self.db_path)
