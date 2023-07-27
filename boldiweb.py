111608fff87a3d96eef79eb1745b6795a7cff7c5
Palotás Boldizsár <boldizsar.palotas@gmail.com>
2023-07-27 18:02:19 +0200

    Set DPI and image quality

--- boldiweb.py
+++ boldiweb.py
@@ -326,13 +326,13 @@ class TargetImageHandler(FileHandler):
         with Image.open(image.path) as pil_image:
             w, h = 3000, round(3000 / pil_image.size[0] * pil_image.size[1])
             with pil_image.resize((w, h)) as resized_image:
-                resized_image.save(image.path_3000w)
+                resized_image.save(image.path_3000w, quality=95, dpi=(240, 240,))
             w, h = 1500, round(1500 / pil_image.size[0] * pil_image.size[1])
             with pil_image.resize((w, h)) as resized_image:
-                resized_image.save(image.path_1500w)
+                resized_image.save(image.path_1500w, quality=95, dpi=(240, 240,))
             w, h = 800, round(800 / pil_image.size[0] * pil_image.size[1])
             with pil_image.resize((w, h)) as resized_image:
-                resized_image.save(image.path_800w)
+                resized_image.save(image.path_800w, quality=95, dpi=(240, 240,))
 
         with open(image.exif_path, "w") as fp:
             json.dump(get_exif_tags(image.source.path), fp, indent=2)
